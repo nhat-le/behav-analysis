53359ba7e2daadcac27f14b47aa94463e9285746
Cherry <ywang37@mit.edu>
2021-09-16 13:39:22 -0400

    Updated single session

--- single_session_visualize.m
+++ single_session_visualize.m
@@ -1,99 +1,42 @@
 
-animal = 'e50';
-id_lst = [14, 24, 55];
+animals = {'e50', 'e54', 'e57', 'f01', 'f02', 'f03', 'f04'};
+id_lst = [36];
 all_states = [2, 3];
+for a = {'f03'} %animals
+    animal = a{1};
+    filedir = ['C:\Users\Cherry Wang\Dropbox (MIT)\Nhat\animalHMMData\animalData_' animal '.mat'];
+%     filedir = 'C:\Users\Cherry Wang\Desktop\UROP-Nhat\HMM\animalData_f16.mat';
+    load(filedir)
 
-[~,computername] = system('hostname');
-switch computername(1:end-1)
-    case 'dhcp-10-29-99-156.dyn.MIT.EDU'
-        rigbox = '/Users/minhnhatle/Dropbox (MIT)/Nhat/Rigbox';
-        savefolder = '/Users/minhnhatle/Dropbox (MIT)/Nhat/Rigbox/HMM';
-    case 'LAPTOP-HGDQ2Q94'
-        rigbox = 'C:\Users\Cherry Wang\Dropbox (MIT)\Nhat\Rigbox';
-        savefolder = 'C:\Users\Cherry Wang\Desktop\UROP-Nhat\HMM';
-    otherwise
-        rigbox = '/Users/minhnhatle/Dropbox (MIT)/Nhat/Rigbox';
-        savefolder = '/Users/minhnhatle/Dropbox (MIT)/Nhat/Rigbox/HMM';
-end
+    states_lst = animalData.states;
+    logllh_lst = animalData.logllh;
+    aic_lst = animalData.aic;
+    bic_lst = animalData.bic;
+    maxdelays = animalData.delays;
+    maxiter = animalData.maxiter;
+    PSTATES_lst = animalData.PSTATES;
+    sessionInfo_lst = animalData.sessionInfo;
+    sessions_lst = animalData.sessions;
 
-root =fullfile(rigbox, animal);
-folders = dir(fullfile(root, '202*'));
+    figure;
+    plot_id = 1;
+    axeslst = [];
+    state_counts = struct;
+    for id = id_lst
+        allchoices = animalData.sessionInfo(id).choices;
+        alltargets = animalData.sessionInfo(id).targets;
 
+        curr_state = 3;
+        PSTATES = PSTATES_lst(id).states3;
 
-sessions_lst = 1:numel(folders);
-loadpath = fullfile(savefolder, ['animalData_' animal '.mat']);
-load(loadpath);
-states_lst = animalData.states;
-logllh_lst = animalData.logllh;
-aic_lst = animalData.aic;
-bic_lst = animalData.bic;
-maxdelays = animalData.delays*10;
-maxiter = animalData.maxiter;
-PSTATES_lst = animalData.PSTATES;
-sessionInfo_lst = animalData.sessionInfo;
-
-figure;
-plot_id = 1;
-axeslst = [];
-for i = 1:numel(id_lst)
-    id = id_lst(i);
-    disp(id)
-    % Concatenate sessions from the same day into one session
-    files = dir(fullfile(root, ...
-        folders(id).name, '*/*Block.mat'));
-    disp(folders(id).name);
-    allchoices = [];
-    alltargets = [];
-    skipping = 0;
-    for file = 1:numel(files)
-        try
-            load(fullfile(files(file).folder, files(file).name));
-        catch
-        end
-        parts = strsplit(block.expDef, '\');
-        filename = parts{end};
-
-        if ~startsWith(filename, 'blockWorld')
-            skipping = 1;
-            fprintf('Skipping id = %d: %s...\n', id, filename);
-        else
-
-%         if ~isfield(block.paramsValues, 'rewardDelay')
-%             skipping = 1;
-%             break;
-%         end
-
-            choices = block.events.responseValues;
-            targets = block.events.contrastLeftValues;
-            N = min([numel(choices) numel(targets)]);
-            allchoices = [allchoices choices(1:N)];
-            alltargets = [alltargets targets(1:N)];
-        end
-    end
-    
-    
-
-    N = numel(allchoices);
-
-    if skipping
-        maxdelays(end+1) = nan;
-    end
-    allchoices(allchoices == 0) = randsample([-1, 1],1);
-    allchoices = (allchoices + 1) / 2 + 1;
-    switches = find(diff(alltargets) ~= 0);
-
-    
-    for state_id = 1:length(all_states)
-        curr_state = all_states(state_id);
-        PSTATES = PSTATES_lst(id).(['states' num2str(curr_state)]);
 
         %% Visualize states of one session
-        l = subplot(numel(id_lst),2,plot_id);
-        axeslst = [axeslst l];
-        
+%         l = subplot(numel(id_lst),1,plot_id);
+%         axeslst = [axeslst l];
+
         EMIS_EST = sessionInfo_lst(id).(['EMIS_EST' num2str(curr_state)]);
-        
-        
+
+
         new_PSTATES = [EMIS_EST(:,1) PSTATES];
         order = sortrows(new_PSTATES, 1);
         new_PSTATES = order(:,2:end);
@@ -101,48 +44,90 @@ for i = 1:numel(id_lst)
         new_I = [0 I 0];
         state_changes = find(diff(new_I)~=0);
         state_changes = [state_changes+1 state_changes];
+        ax1 = subplot(311);
+        plot(allchoices-1, 'ko', 'MarkerSize', 4)
         
-        plot(allchoices, 'ko', 'MarkerSize', 4)
+        title('Underfitted HMM', 'FontSize', 15);
+        ax2 = subplot(312);
+        plot(allchoices-1, 'ko', 'MarkerSize', 4)
         hold on
-        plot((new_PSTATES' + 1))
+%         plot(new_PSTATES', 'LineWidth', 2, 'Color', 'blue')
+%         hold on
+        graph_states = new_PSTATES';
+        
+        plot(graph_states(1:end,1), 'LineWidth', 2, 'Color', 'blue')
         hold on
+        plot(graph_states(1:end, 2), 'LineWidth', 2, 'Color', 'cyan')
+        hold on
+        plot(graph_states(1:end, 3), 'LineWidth', 2, 'Color', 'yellow')
+        hold on
+        hold off
+        
         
         colors = {'blue', 'cyan', 'yellow'};
+        ax3 = subplot(313);
+        plot(allchoices-1, 'ko', 'MarkerSize', 4)
+        plot(graph_states(1:end,1), 'LineWidth', 2, 'Color', 'blue')
+        hold on
+        plot(graph_states(1:end, 2), 'LineWidth', 2, 'Color', 'cyan')
+        hold on
+        plot(graph_states(1:end, 3), 'LineWidth', 2, 'Color', 'yellow')
+        hold on
         
-        for s=1:all_states(state_id)
+        
+        
+        for s=1:3
             state_s = find(new_I==s);
             coords = state_changes(ismember(state_changes, state_s));
             coords = coords - 1;
             x = reshape(coords, fix(numel(coords)/2), 2);
-            new_x = [x'; x'];
+           new_x = [x'; x'];
+%             new_x = x';
+            state_counts(id).(['state' num2str(s)]) = sum(new_x(2,:) - new_x(1,:))/(numel(x)/2);
             x = [new_x(1:2,:); new_x(4,:); new_x(3,:)];
             y = zeros(size(x));
-            y(1:2,:) = 1;
-            y(3:4,:) = 2;
+            y(1:2,:) = 0;
+            y(3:4,:) = 1;
             fill(x,y,colors{s},'FaceAlpha',.5);
         end
-        vline(switches, 'k--')
-        %vline(subjSwitches, 'r--');
+%         lgd = legend('Choices', 'State 1 (right)', 'State 2 (explore)', 'State 3 (left)');
+%         lgd.FontSize = 10;
+%         vline(switches, 'k--')
+%         vline(subjSwitches, 'r--');
         ylim([-1 3])
-        %title([animal '(' folders(id).name ') session #' num2str(id)]);
-        date = strrep(folders(id).name, '-','');
-        ylabel(['#' num2str(id) '(' date(3:end) ')'])
-
+        xlabel('Trials')
         plot_id = plot_id+1;
+
+    linkaxes([ax1 ax2 ax3],'y')
     end
-
     
-end
-linkaxes(axeslst,'xy')
+    %allavg = [state_counts.state1] + [state_counts.state2] + [state_counts.state3];
+%     figure;
+%     sgtitle(animal);
+%     ax1=subplot(211);
+    
+%     plot(maxdelays, 'linewidth', 4);
+%     hold on;
+%     plot([state_counts.state1], 'blue');
+%     hold on;
+%     plot([state_counts.state2], 'cyan');
+%     hold on;
+%     plot([state_counts.state3], 'red');
 
-%%
-function [Test, Eest, status] = hmmtrainRobust(data, Tinit, Einit, maxiter)
-try
-    [Test, Eest] = hmmtrain(data, Tinit, Einit, 'Maxiterations', maxiter);
-    status = 0;
-catch 
-    Test = nan;
-    Eest = nan;
-    status = 1;
-end
+
+%     ax2=subplot(212);
+%     bias = ([state_counts.state3]-[state_counts.state1]);
+%     plot(bias, 'linewidth', 3);
+%     hold on;
+%     explore = [state_counts.state2];
+%     
+%     noexplore = ([state_counts.state3]+[state_counts.state1])/2;
+%     plot(explore-noexplore, 'linewidth', 3);
+%     hold on;
+%     %hline(0, 'k-');
+% %     plot(noexplore, 'linewidth', 3);
+%     legend('bias', 'explore');
+%     
+%     
+%     linkaxes([ax1, ax2],'x')
 end
